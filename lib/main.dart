8f3515d29322ce523e73891267b52ca333251161
Braga <fcobbraga@gmail.com>
2019-09-26 11:41:47 -0300

    Aula stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,74 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(
-    title: "Contador de Pessoas",
-    home: Column(
+void main() {
+  runApp(MaterialApp(title: "Contador de Pessoas", home: home()));
+}
+
+class home extends StatefulWidget {
+  @override
+  _homeState createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  int _pessoa = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
       children: <Widget>[
-        Text(".", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        Image.asset(
+          "images/marvel.jpg",
+          fit: BoxFit.cover,
+          height: 1000.0,
         ),
-        FlatButton(
-            child: Text("."),
-            onPressed: (){}
-        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "$_pessoa",
+              style: TextStyle(
+                  color: Colors.orange,
+                  fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                      child: Text("Botão +",
+                          style: TextStyle(
+                              color: Colors.orange,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30)),
+                      onPressed: () {
+                        debugPrint("mais 1");
+                      }),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                      child: Text("Botão -",
+                          style: TextStyle(
+                              color: Colors.purple,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30)),
+                      onPressed: () {
+                        debugPrint("menos 1");
+                      }),
+                ),
+              ],
+            ),
+            Text(
+              "",
+              style: TextStyle(
+                  color: Colors.orange,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 1),
+            ),
+          ],
+        )
       ],
-    )
-  ));
+    );
+  }
 }
